2b228804aa66785064a273c064581c590c11bac5
Dongmin Lee <donglmin@icloud.com>
2021-11-23 11:19:38 -0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World - edited to trigger new release"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
