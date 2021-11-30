9dcc561bdfdcf11cf89aa11fb89caadb01938053
dongl <dmlee533@gmail.com>
2021-11-29 19:56:15 -0800

    edited to serve index.html

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,12 +12,30 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-   	        w.Write([]byte("<h1>Hello World</h1> <br> <p>Cloud Project</p>"))
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
-		fmt.Println("Hello world - the log message")
-	})
+	log.Println("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func main() {
+	fs := http.FileServer(http.Dir("./index.html"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :8080...")
 	http.ListenAndServe(":8080", nil)
+
+	// staticHandler := http.FileServer(http.Dir("./assets"))
+	// mux.Handle("/assets/", http.StripPrefix("/assets/", staticHandler))
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  //  	        w.Write([]byte("<h1>Hello World</h1> <br> <p>Cloud Project</p>"))
+	//
+	// 	fmt.Println("Hello world - the log message")
+	// })
+	// http.ListenAndServe(":8080", nil)
 }
 
 type response struct {
